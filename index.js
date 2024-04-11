60a00a7bb136c1ba7f70ae16572c2dfc25ac9ebd
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-04-11 21:54:33 +0100

    toggle modal feature

---
+++ index.js
@@ -0,0 +1,39 @@
+// template_4194osl
+
+// service_tktqlnj
+
+// JHVbSbrPwuLT-kqFi
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_tktqlnj",
+      "template_4194osl",
+      event.target,
+      "JHVbSbrPwuLT-kqFi"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on ryamacoding@outlook.com"
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if(isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
